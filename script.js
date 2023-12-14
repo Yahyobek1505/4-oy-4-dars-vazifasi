481c56eae7b9bad0646379a30bdfcedbe3ad3c4c
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-14 16:24:38 +0500

    Homework is almost  done.

---
+++ script.js
@@ -0,0 +1,87 @@
+/* <======================> 1-Masala <=====================>*/
+// let num1 = prompt("Istalgan sonni kirting")
+// let counter = 0;
+// for (let i = 1; i <= num1; i++) {
+//   if (i % 3 == 0 && i % 7 == 0) {
+//     counter++;
+//     console.log(i);
+//   }
+// }
+// alert(counter);
+// console.log(counter);
+
+/* <======================> 2-Masala <=====================>*/
+// let num1 = 20 //prompt("Istalgan sonni kirting")
+// let counter = 0;
+// for (let i = num1; i >= 1; i--) {
+//   if (i % 2 == 0) {
+//     counter += i;
+//     console.log(i);
+//   }
+// }
+// // alert(counter);
+// console.log(counter);
+/* <======================> 3-Masala <=====================>*/
+// let num1 = 100 //prompt("Istalgan sonni kirting")
+// let counter = 0;
+// for (let i = 1; i <=num1 ; i++) {
+//   if (i % 5 == 0 && i % 7 != 0)  {
+//     counter ++;
+//     console.log(i);
+//   }
+// }
+// // alert(counter);
+// console.log("Jami" + " " + counter + " ta son");
+
+/* <======================> 4-Masala <=====================>*/
+// let a = -40 //prompt("Istalgan sonni kirting")
+// let b = 1 //prompt("Istalgan sonni kirting")
+// let counter = 0;
+
+// for (let i = a; i <=b ; i++) {
+//     if (i < 0)  {
+//       counter += i;
+//       console.log(i);
+//     }
+//   }
+//   // // alert(counter);
+// console.log(counter);
+
+/* <======================> 5-Masala <=====================>*/
+
+// let a = -40 //prompt("Istalgan sonni kirting")
+// let b = 1 //prompt("Istalgan sonni kirting")
+// let counter = 0;
+
+// for (let i = a; i <=b ; i++) {
+//     if (i < 0)  {
+//       counter += i;
+//       console.log(i);
+//     }
+//   }
+//   // // alert(counter);
+// console.log(counter);
+
+/* <======================> 6-Masala <=====================>*/
+
+// let num1 = 5; //prompt("Istalgan sonni kirting");
+// let counter = 1;
+// for (let i = num1; i >= 1; i--) {
+//     counter *= i;
+//     console.log(i);
+// }
+// // alert(counter);
+// console.log(counter);
+
+/* <======================> 9-Masala <=====================>*/
+
+let num1 = 10; //prompt("Istalgan sonni kirting");
+let counter = 1;
+for (let i = 1; i <= num1; i++) {
+  if (i ** 2) {
+        counter += i;
+        console.log(i);
+  }
+}
+// // alert(counter);
+console.log(counter);
