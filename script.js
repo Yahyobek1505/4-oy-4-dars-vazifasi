8bea544f1536525d057b44a609d2110a28284397
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-14 23:13:28 +0500

    Homework is done.

--- script.js
+++ script.js
@@ -73,15 +73,56 @@
 // // alert(counter);
 // console.log(counter);
 
+/* <======================> 8-Masala <=====================>*/
+
+// let num1 = 4; //prompt("Istalgan sonni kirting");
+// let counter = 0;
+// for (let i = 1; i <= num1; i++) {
+//   if (num1 % i == 0) {
+//     counter++;
+//   }
+// }
+// if (counter == 2) {
+//   console.log( " Bu tub son " + " " + num1)
+//   // alert(" Bu tub son" + " " + num1);
+
+// } else {
+//   console.log( " Bu tub son emas " + " " + num1)
+//   // alert(" Bu tub son emas " + " " + num1);
+// }
+// console.log(counter);
+
 /* <======================> 9-Masala <=====================>*/
 
-let num1 = 10; //prompt("Istalgan sonni kirting");
-let counter = 1;
-for (let i = 1; i <= num1; i++) {
-  if (i ** 2) {
-        counter += i;
-        console.log(i);
-  }
-}
-// // alert(counter);
-console.log(counter);
+// let num1 = 4; //prompt("Istalgan sonni kirting");
+// let counter = 0;
+// for (let i = 1; i <= num1; i++) {
+//   i ** 2;
+//   counter += i;
+// }
+// console.log(counter);
+// // // alert(counter);
+
+/* <======================> 10-Masala <=====================>*/
+
+// let num1 = 5; //prompt("Istalgan sonni kirting");
+// let counter = 0;
+// let tubSonlar =0;
+// for (let i = 1; i <= num1; i++) {
+//   if (num1 % i == 0) {
+//     counter++;
+
+//   }
+// }
+// if (counter == 2) {
+  
+// for (let i = counter; i <= num1; i++) {
+//    tubSonlar += counter;
+//    // alert(" Bu tub son" + " " + num1);
+//   }
+//   console.log(tubSonlar)
+// } else {
+//   console.log( " Bu tub son emas " + " " + num1)
+// //   // alert(" Bu tub son emas " + " " + num1);
+// }
+// // console.log(counter);
